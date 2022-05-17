7ef32cba88019fdc812a29f9fa31a86dd34c03c3
조은총 <whdmschd2@gmail.com>
2022-05-18 02:30:04 +0900

    Add: coin data fetch

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -34,15 +34,29 @@ interface RouterState {
 const Coin = () => {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
-
   const { state } = useLocation() as RouterState;
-  console.log(state.name);
+
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await await fetch(
+        `https://api.coinpaprika.com/v1/tickers/${coinId}`
+      );
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>"Loading..."</Loader> : null}
+      {loading ? <Loader>"Loading..."</Loader> : <span>null</span>}
     </Container>
   );
 };
