164dfd4b836dfb9a6e7639f2abdf1a214e792b1a
조은총 <whdmschd2@gmail.com>
2022-05-18 06:19:15 +0900

    Modfiy: Detail Coin page data fetch by react query

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,20 @@
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
 export const fetchCoins = async () => {
-  const responseData = await (
-    await fetch("https://api.coinpaprika.com/v1/coins")
-  ).json();
+  const responseData = await (await fetch(`${BASE_URL}/coins`)).json();
   return responseData.slice(0, 100);
+};
+
+export const fetchCoinInfo = async (coinId: string) => {
+  const responseData = await (
+    await fetch(`${BASE_URL}/coins/${coinId}`)
+  ).json();
+  return responseData;
+};
+
+export const fetchCoinTickers = async (coinId: string) => {
+  const responseData = await (
+    await fetch(`${BASE_URL}/tickers/${coinId}`)
+  ).json();
+  return responseData;
 };
