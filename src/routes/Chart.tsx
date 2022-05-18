71c8deab8900c2de5342de8f3cf123ba7fe0afbc
Mcdonalds Trump <whdmschd2@gmail.com>
2022-05-18 23:16:51 +0900

    Add: chart by apexcharts

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactApexChart from "react-apexcharts";
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "./api";
@@ -7,7 +8,7 @@ interface ChartProps {
   coinId: string;
 }
 
-interface ICoinHistory {
+interface IHistorical {
   time_open: Date;
   time_close: Date;
   open: number;
@@ -20,11 +21,71 @@ interface ICoinHistory {
 
 const Chart = () => {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery<ICoinHistory[]>(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   console.log(data ? data[0] : null);
-  return <div>Chart</div>;
+  console.log(data);
+
+  const dateXaxis = data?.map((a) => {
+    const date = new Date(a.time_close);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return day;
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            xaxis: {
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              categories: dateXaxis,
+              labels: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
